7d74a1a97832cebbd8a7e259f7b83277ef816f46
alannabutterfield <81856007+alannabutterfield@users.noreply.github.com>
2021-04-03 20:49:48 +1030

    index.js

---
+++ Index.js
@@ -0,0 +1,5 @@
+const Discord = require("discord.js"); // Importando o Discord.js
+const client = new Discord.Client(); 
+
+client.login("ODE4Njk2NzM2Njc5NjU3NDkz.YEb0wA.K3wsWg-HRvjih6578mWBEeRns6I
+");
